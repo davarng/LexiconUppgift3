95e7850bd2901cf398a4a37292554a49bd50f712
davarng <113583854+davarng@users.noreply.github.com>
2025-04-08 22:55:51 +0200

    Answered questions

--- LexiconUppgift3/Program.cs
+++ LexiconUppgift3/Program.cs
@@ -8,17 +8,11 @@ internal class Program
     //Frågor
     //1. Det går ej. Man får ett kompilatorfel som säger "Argument 1: cannot convert from 'LexiconUppgift3.Vehicles.Car' to 'LexiconUppgift3.Vehicles.Motorcycle'"
     //2. Den borde vara av typen vehicle. List<Vehicle>
-    //3. 
+    //3. Nej det går ej. Clean finns ej i Vehicle. "'Vehicle' does not contain a definition for 'Clean' and no accessible extension method 'Clean' accepting a first argument of type 'Vehicle' could be found (are you missing a using directive or an assembly reference?)"
+    //4. Truck och Car som jag har implementerat ICleanable på ärver redan från Vehicle. I C# så kan klasser ej ärva från flera klasser.
+    //Därför kan man använda interfaces för att hjälpa dela egenskaper hos klasser som redan ärver.
     static void Main(string[] args)
     {
-        List<Vehicle> list = new List<Vehicle>();
-        Car car = new Car("toyota","yaris",1998,1400.0);
-        Truck truck = new Truck("toyota", "pickup", 1988, 3000.0);
-        list.Add(car);
-        list.Add(truck);
-        list[0].Clean();
-
-
         List<Vehicle> vehicleList = new List<Vehicle>();
         while (true) 
         {
